d4f35c17297878705372e14f9273174dcfc8a9b2
David <daso.timileyin@gmail.com>
2023-09-30 10:08:05 +0100

    payload

---
+++ payload.js
@@ -0,0 +1,14 @@
+
+// {
+//     "id":4,
+
+//     "accuracy": 7299.612787273156,
+//     "altitude": null,
+//     "altitudeAccuracy": null,
+//     "heading": null,
+//     "latitude": 61.5568768,
+//     "longitude": 30.3488896,
+//     "speed": null,
+//     "socketID": "PeC3HZWsJCjQ8kXjAAAD",
+//     "timestamp": 1665474267691
+// }
